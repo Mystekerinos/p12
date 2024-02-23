2c951d8778086ea448e4abf4117847182faefcfd
mystekerinos <jordy.saintalbin@gmail.com>
2024-02-23 12:50:13 +0100

    fix-title

--- projet12/Front-end/src/DashBoard/DashBoard.jsx
+++ projet12/Front-end/src/DashBoard/DashBoard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import SideBar from "../Components/SideBar/SideBar";
+import "./DashBoard.css";
 
 const DashBoard = () => {
   return (
