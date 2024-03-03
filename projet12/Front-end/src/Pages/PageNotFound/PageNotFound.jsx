48416b338aa382275d67591818258a81b09d1066
mystekerinos <jordy.saintalbin@gmail.com>
2024-03-03 11:12:48 +0100

    fix-design-not-page-found3

--- projet12/Front-end/src/Pages/PageNotFound/PageNotFound.jsx
+++ projet12/Front-end/src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import PageWrapper from "../../Components/PageWrapper/PageWrapper";
 import { Link } from "react-router-dom";
 import "./PageNotFound.css";
 
 const PageNotFound = () => {
   return (
-    <PageWrapper>
+    <div className="page-not-found">
       <h1 className="">404</h1>
       <h2 className="subTitle">Oups! La page que </h2>
       <h2 className="subTitle">vous demandez n'existe pas.</h2>
       <div className="return-to-home">
         <Link to="/">Retourner sur la page d'accueil</Link>
       </div>
-    </PageWrapper>
+    </div>
   );
 };
 
