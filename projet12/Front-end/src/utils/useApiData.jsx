d013a782723b4d4f6c945a5284226ce08b12b724
mystekerinos <jordy.saintalbin@gmail.com>
2024-03-30 18:48:41 +0100

    fix-typing-api-local-scope

--- projet12/Front-end/src/utils/useApiData.jsx
+++ projet12/Front-end/src/utils/useApiData.jsx
@@ -2,10 +2,64 @@ import { useState, useEffect } from "react";
 import { fetchData } from "./FetchData";
 
 const useApiData = (userId) => {
-  const [userData, setUserData] = useState(undefined);
-  const [performanceData, setPerformanceData] = useState(undefined);
-  const [activityData, setActivityData] = useState(undefined);
-  const [averageSessions, setAverageSessions] = useState(undefined);
+  const [userData, setUserData] = useState({
+    id: -1,
+    userInfos: {
+      firstName: "",
+      lastName: "",
+      age: -1,
+    },
+    todayScore: 0.0,
+    keyData: {
+      calorieCount: 0,
+      proteinCount: -1,
+      carbohydrateCount: -1,
+      lipidCount: -1,
+    },
+  });
+  const [performanceData, setPerformanceData] = useState({
+    userId: 0,
+    kind: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+    },
+    data: [
+      { value: 0, kind: -1 },
+      { value: 0, kind: -1 },
+      { value: 0, kind: -1 },
+      { value: 0, kind: -1 },
+      { value: 0, kind: -1 },
+      { value: 0, kind: -1 },
+    ],
+  });
+  const [activityData, setActivityData] = useState({
+    userId: -1,
+    sessions: [
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+      { day: "", kilogram: 0, calories: -1 },
+    ],
+  });
+  const [averageSessions, setAverageSessions] = useState({
+    userId: 0,
+    sessions: [
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+      { day: 0, sessionLength: -1 },
+    ],
+  });
 
   useEffect(() => {
     fetchData(`/user/${userId}`)
