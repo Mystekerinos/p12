435e64be01b491fe075cfc8bb1ee19bc7d7c76d1
mystekerinos <jordy.saintalbin@gmail.com>
2024-04-07 09:04:19 +0200

    remove-getUserData

--- projet12/Front-end/src/models/userPerformanceData.jsx
+++ projet12/Front-end/src/models/userPerformanceData.jsx
@@ -6,7 +6,7 @@ export default class UserPerformanceData {
   /**
    * Create a user performance data object.
    * @constructor
-   *
+   
    * @param {Object} data - The performance data of the user.
    */
   constructor(data) {
